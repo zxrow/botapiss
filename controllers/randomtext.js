e815dba53e1b2bffa4ca5e14e8be88fd4dee17d3
ZxagungX <49530313+zx-guard@users.noreply.github.com>
2022-12-30 13:46:13 +0700

    Initial commit

---
+++ controllers/randomtext.js
@@ -0,0 +1,195 @@
+const { cekKey } = require('../database/db');
+const { readFileTxt, readFileJson } = require('../lib/function');
+
+async function cakLontong(req, res) {
+    const apikey = req.query.apikey;
+    if (apikey === undefined) return res.status(404).send({
+        status: 404,
+        message: `Input Parameter apikey`
+    });
+    const check = await cekKey(apikey);
+    if (!check) return res.status(403).send({
+        status: 403,
+        message: `apikey ${apikey} not found, please register first!`
+    });
+    readFileJson('./lib/data/caklontong.json').then(result => {
+        res.status(200).send({
+            status: 200, 
+            question: result.quiz, 
+            answer: result.answer, 
+            detail: result.detail
+        });
+    }).catch(error => {
+        console.log(error);
+        res.status(500).send({status: 500, message: 'Internal Server Error'});
+    });
+}
+
+async function tebakgambar(req, res) {
+    const apikey = req.query.apikey;
+    if (apikey === undefined) return res.status(404).send({
+        status: 404,
+        message: `Input Parameter apikey`
+    });
+    const check = await cekKey(apikey);
+    if (!check) return res.status(403).send({
+        status: 403,
+        message: `apikey ${apikey} not found, please register first!`
+    });
+    readFileJson('./lib/data/tebakgambar.json').then(result => {
+        res.status(200).send({
+            status: 200, 
+            img: result.img, 
+            jawaban: result.jawaban,
+            deskripsi: result.deskripsi
+        });
+    }).catch(error => {
+        console.log(error);
+        res.status(500).send({status: 500, message: 'Internal Server Error'});
+    });
+}
+
+async function pemandangananimv1(req, res) {
+    const apikey = req.query.apikey;
+    if (apikey === undefined) return res.status(404).send({
+        status: 404,
+        message: `Input Parameter apikey`
+    });
+    const check = await cekKey(apikey);
+    if (!check) return res.status(403).send({
+        status: 403,
+        message: `apikey ${apikey} not found, please register first!`
+    });
+    readFileJson('./lib/data/pemandangananimv1.json').then(result => {
+        res.status(200).send({
+            status: 200, 
+            img: result.url
+        });
+    }).catch(error => {
+        console.log(error);
+        res.status(500).send({status: 500, message: 'Internal Server Error'});
+    });
+}
+
+async function pemandangananimv2(req, res) {
+    const apikey = req.query.apikey;
+    if (apikey === undefined) return res.status(404).send({
+        status: 404,
+        message: `Input Parameter apikey`
+    });
+    const check = await cekKey(apikey);
+    if (!check) return res.status(403).send({
+        status: 403,
+        message: `apikey ${apikey} not found, please register first!`
+    });
+    readFileJson('./lib/data/pemandangananimv2.json').then(result => {
+        res.status(200).send({
+            status: 200, 
+            img: result.url
+        });
+    }).catch(error => {
+        console.log(error);
+        res.status(500).send({status: 500, message: 'Internal Server Error'});
+    });
+}
+
+async function quotes(req, res) {
+    const apikey = req.query.apikey;
+    if (apikey === undefined) return res.status(404).send({
+        status: 404,
+        message: `Input Parameter apikey`
+    });
+    const check = await cekKey(apikey);
+    if (!check) return res.status(403).send({
+        status: 403,
+        message: `apikey ${apikey} not found, please register first!`
+    });
+    readFileJson('./lib/data/quotes.json').then(result => {
+        res.status(200).send({
+            status: 200, 
+            quotes: result.quotes, 
+            author: result.author
+        });
+    }).catch(error => {
+        console.log(error);
+        res.status(500).send({status: 500, message: 'Internal Server Error'});
+    });
+}
+
+async function fakta(req, res) {
+    const apikey = req.query.apikey;
+    if (apikey === undefined) return res.status(404).send({
+        status: 404,
+        message: `Input Parameter apikey`
+    });
+    const check = await cekKey(apikey);
+    if (!check) return res.status(403).send({
+        status: 403,
+        message: `apikey ${apikey} not found, please register first!`
+    });
+    readFileTxt('./lib/data/fakta.txt').then(result => {
+        res.status(200).send({status: 200, result: result});
+    }).catch(error => {
+        console.log(error);
+        res.status(500).send({status: 500, message: 'Internal Server Error'});
+    });
+}
+
+async function bijak(req, res) {
+    const apikey = req.query.apikey;
+    if (apikey === undefined) return res.status(404).send({
+        status: 404,
+        message: `Input Parameter apikey`
+    });
+    const check = await cekKey(apikey)
+    if (!check) return res.status(403).send({
+        status: 403,
+        message: `apikey ${apikey} not found, please register first!`
+    });
+    readFileTxt('./lib/data/bijak.txt').then(result => {
+        res.status(200).send({status: 200, result: result});
+    }).catch(error => {
+        console.log(error);
+        res.status(500).send({status: 500, message: 'Internal Server Error'});
+    });
+}
+
+async function ptl(req, res) {
+    const apikey = req.query.apikey;
+    if (apikey === undefined) return res.status(404).send({
+        status: 404,
+        message: `Input Parameter apikey`
+    });
+    const check = await cekKey(apikey);
+    if (!check) return res.status(403).send({
+        status: 403,
+        message: `apikey ${apikey} not found, please register first!`
+    });
+    readFileTxt('./lib/data/ptl.txt').then(result => {
+        res.status(200).send({status: 200, result: result});
+    }).catch(error => {
+        console.log(error);
+        res.status(500).send({status: 500, message: 'Internal Server Error'});
+    });
+}
+
+async function motivasi(req, res) {
+    const apikey = req.query.apikey;
+    if (apikey === undefined) return res.status(404).send({
+        status: 404,
+        message: `Input Parameter apikey`
+    });
+    const check = await cekKey(apikey);
+    if (!check) return res.status(403).send({
+        status: 403,
+        message: `apikey ${apikey} not found, please register first!`
+    });
+    readFileTxt('./lib/data/motivasi.txt').then(result => {
+        res.status(200).send({status: 200, result: result.replace(/"/g, '')});
+    }).catch(error => {
+        console.log(error);
+        res.status(500).send({status: 500, message: 'Internal Server Error'});
+    });
+}
+
+module.exports = {cakLontong, quotes, bijak, fakta, ptl, motivasi, tebakgambar, pemandangananimv1, pemandangananimv2 };
---
+++ routes/api.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const router = express.Router();
+const { cekKey } = require('../database/db'); 
+const { youtubePlay, youtubeMp4, youtubeMp3 } = require('../controllers/yt');
+const { cakLontong, bijak, quotes, fakta, ptl, motivasi, tebakgambar, pemandangananimv2, pemandangananimv1 } = require('../controllers/randomtext');
+
+router.get('/checkkey', async (req, res) => {
+    const apikey = req.query.apikey;
+    if (apikey === undefined) return res.status(404).send({
+        status: 404,
+        message: `Input Parameter apikey`
+    });
+    const check = await cekKey(apikey);
+    if (!check) return res.status(403).send({
+        status: 403,
+        message: `apikey ${apikey} not found, please register first!`
+    });
+    res.send({status: 200, apikey: apikey, response: 'Active'});
+});
+
+router.get('/ytplay', youtubePlay);
+
+router.get('/ytmp4', youtubeMp4);
+
+router.get('/ytmp3', youtubeMp3);
+
+router.get('/caklontong', cakLontong);
+
+router.get('/tebakgambar', tebakgambar);
+
+router.get('/pemandangananimv2', pemandangananimv2);
+
+router.get('/pemandangananimv1', pemandangananimv1);
+
+router.get('/quotes', quotes);
+
+router.get('/fakta', fakta);
+
+router.get('/bijak', bijak);
+
+router.get('/ptl', ptl);
+
+router.get('/motivasi', motivasi);
+
+module.exports = router;
